9255cd50bfc1f85530881a2467b8a21418a68cf0
Dmytro Yermolenko <dmytro.yermolenko@dev-pro.net>
2018-04-23 16:19:14 +0300

    App Containers

--- src/conatiners/App.tsx
+++ src/conatiners/App.tsx
@@ -1,23 +1,89 @@
 import * as React from 'react';
+
 import './App.css';
 
-import logo from '../logo.svg';
+import {connect} from "react-redux";
 
-import Battlefield from './Battlefield';
+import Battlefield from "./Battlefield";
 
-class App extends React.Component {
+import * as actions from '../actions';
+import {StoreState} from "../reducers";
+
+import {PlayerArea} from "../core/PlayerArea";
+import {DotShip} from "../core/ship/DotShip";
+import {IShip} from "../core/ship/IShip";
+import {LShip} from "../core/ship/LShip";
+
+export interface Props {
+  playerField: number[][];
+  numberOfMoves: number;
+  isGameEnded: boolean;
+  startGame?: (ships: any[], options: {}) => void;
+  makeAMove?: (x: number, y: number) => void;
+}
+
+class App extends React.Component<Props> {
+  private makeAMove: (e: Event) => void;
+
+  constructor(props: Props) {
+    super(props);
+    this.startGame = this.startGame.bind(this);
+  }
+
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Battleships</h1>
         </header>
 
-        <Battlefield />
+        <h2>
+          {this.props.isGameEnded ? (
+            `Congratulations, you've destroyed all ships.`
+          ) : (
+            `Press any key to shoot. (${this.props.numberOfMoves})`
+          )}
+        </h2>
+
+        <section className="App-body">
+          <Battlefield />
+        </section>
+
+        <button onClick={this.startGame}>Start game</button>
       </div>
     );
   }
+
+  public startGame() {
+    this.componentWillUnmount();
+
+    this.props.startGame!([LShip, IShip, DotShip, DotShip], {width: 10, height: 10});
+
+    this.makeAMove = () => {
+      const {x, y} = (new PlayerArea(this.props.playerField)).randomEmptyCell();
+
+      this.props.makeAMove!(x, y);
+    };
+
+    document.addEventListener('click', this.makeAMove);
+    document.addEventListener('keydown', this.makeAMove);
+  }
+
+  public componentWillUpdate(nextProps: Props) {
+    if (nextProps.isGameEnded !== this.props.isGameEnded && nextProps.isGameEnded) {
+      this.componentWillUnmount();
+    }
+  }
+
+  public componentWillUnmount() {
+    document.removeEventListener('click', this.makeAMove);
+    document.removeEventListener('keydown', this.makeAMove);
+  }
 }
 
-export default App;
+export default connect<Props>(({numberOfMoves, isGameEnded, playerField}: StoreState): Props => {
+  return {numberOfMoves, isGameEnded, playerField};
+}, {
+  startGame: (ships: any[], options: {}) => actions.startGame(ships, options),
+  makeAMove: (x: number, y: number) => actions.makeAMove(x, y)
+})(App);
--- src/conatiners/Battlefield.tsx
+++ src/conatiners/Battlefield.tsx
@@ -1,24 +1,7 @@
-import * as actions from '../actions/';
+import {connect} from 'react-redux';
+import Battlefield, {Props} from '../components/Battlefield';
+import {StoreState} from "../reducers";
 
-import { connect } from 'react-redux';
-import { StoreState } from '../actions/types';
-import Battlefield from '../components/Battlefield';
-
-export function mapStateToProps({ playerField, numberOfMoves }: StoreState) {
-  return {
-    playerField,
-    numberOfMoves,
-  }
-}
-
-// export function mapDispatchToProps(dispatch: Dispatch<actions.EnthusiasmAction>) {
-//   return {
-//     onIncrement: () => dispatch(actions.incrementEnthusiasm()),
-//     onDecrement: () => dispatch(actions.decrementEnthusiasm()),
-//   }
-// }
-
-export default connect(mapStateToProps, {
-  onIncrement: () => actions.incrementEnthusiasm(),
-  onDecrement: () => actions.decrementEnthusiasm(),
+export default connect(({playerField}: StoreState): Props => {
+  return {playerField};
 })(Battlefield);
